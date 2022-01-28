6d62c4323b9137b7cc0cd1dd76c867286074bd36
Rajesh Deo <rajesh.deo@gmail.com>
2022-01-28 21:11:45 +0530

    Update to move from url() to re_path() for as per Django > 3.x releases.

--- board/urls.py
+++ board/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.urls import path, include, re_path
+#from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
@@ -26,68 +26,68 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # About
-    url(r'^about/$', views.about, name='about'),
+    re_path(r'^about/$', views.about, name='about'),
 
     # homepage
-    url(r'^$', views.BoardListView.as_view(), name='home'),
+    re_path(r'^$', views.BoardListView.as_view(), name='home'),
 
     # board topics
-    url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
 
     # new topics
-    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 
     # signup and profile view
-    url(r'^signup/$', accounts_views.signup, name='signup'),
-    url(r'^settings/account/$',
+    re_path(r'^signup/$', accounts_views.signup, name='signup'),
+    re_path(r'^settings/account/$',
         accounts_views.UserUpdateView.as_view(), name='my_account'),
 
     # social auth
-    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
 
     # login / logout
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 
     # Password Reset
-    url(r'^reset/$',
+    re_path(r'^reset/$',
         auth_views.PasswordResetView.as_view(
             template_name='password_reset.html',
             email_template_name='password_reset_email.html',
             subject_template_name='password_reset_subject.txt'
         ), name='password_reset'),
-    url(r'^reset/done/$',
+    re_path(r'^reset/done/$',
         auth_views.PasswordResetDoneView.as_view(
             template_name='password_reset_done.html'),
         name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(
             template_name='password_reset_confirm.html'),
         name='password_reset_confirm'),
-    url(r'^reset/complete/$',
+    re_path(r'^reset/complete/$',
         auth_views.PasswordResetCompleteView.as_view(
             template_name='password_reset_complete.html'),
         name='password_reset_complete'),
 
     # Change password routes
-    url(r'^settings/password/$',
+    re_path(r'^settings/password/$',
         auth_views.PasswordChangeView.as_view(
             template_name='password_change.html'),
         name='password_change'),
-    url(r'^settings/password/done/$',
+    re_path(r'^settings/password/done/$',
         auth_views.PasswordChangeDoneView.as_view(
             template_name='password_change_done.html'),
         name='password_change_done'),
 
     # Topic post view route
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$',
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$',
         views.PostListView.as_view(), name='topic_posts'),
 
     # Post reply route
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$',
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$',
         views.reply_topic, name='reply_topic'),
 
     # Edit post
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
         views.PostUpdateView.as_view(), name='edit_post'),
 ]
